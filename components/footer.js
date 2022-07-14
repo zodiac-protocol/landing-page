517d095c599695d899d364ceceec53d81ccc35e5
Progenitus <progenitus@Progenituss-MacBook-Pro.local>
2022-07-13 20:17:00 -0400

    feat: add style and links to the footer.

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+
+import Link from 'next/link'
+import TwitterLogo from './twitter.svg'
+import DiscordLogo from './discord.svg'
+import TelegramLogo from './telegram.svg'
+
+export function Footer() {
+    return (
+        <footer className="footer">
+            <Link href="/about">About Us</Link>
+            <Link href="/community">Community</Link>
+            <Link href="/media">Media</Link>
+            <Link href="/faq">FAQ</Link>
+            <a href="https://mobile.twitter.com/zodiacprotocol" target="_blank" rel="noopener noreferrer">
+                <TwitterLogo />
+            </a>
+            <a href="https://t.me/Zodiac_Protocol_Announcements" target="_blank" rel="noopener noreferrer">
+                <TelegramLogo />
+            </a>
+            <DiscordLogo />
+        </footer>
+    )
+}
