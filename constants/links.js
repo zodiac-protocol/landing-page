d2c0f2433ef3d81317db2152e69dd2ef512b3c9d
pasdesujin <pasdesujin@gmail.com>
2022-09-06 23:45:04 -0400

    fix: use a correct telegram link for zodiac protocol

--- constants/links.js
+++ constants/links.js
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
     TWITTER: 'https://mobile.twitter.com/zodiacprotocol',
-    TELEGRAM: 'https://t.me/Zodiac_Protocol_Announcements',
+    TELEGRAM: 'https://t.me/Zodiac_Protocol',
     DISCORD: 'https://discord.com/invite/feJtwxdvsQ',
     MEDIUM: 'https://medium.com/@zodiacprotocol'
 }
