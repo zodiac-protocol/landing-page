fa94e42f88e17009d6d238510feacd1cefce3341
jc <johnjchang@gmail.com>
2022-09-08 16:38:45 -0400

    Update links.js

--- constants/links.js
+++ constants/links.js
@@ -1,5 +1,5 @@
 export const SOCIAL_LINKS = {
-    TWITTER: 'https://mobile.twitter.com/zodiacprotocol',
+    TWITTER: 'https://twitter.com/zodiacprotocol',
     TELEGRAM: 'https://t.me/Zodiac_Protocol',
     DISCORD: 'https://discord.com/invite/feJtwxdvsQ',
     MEDIUM: 'https://medium.com/@zodiacprotocol'
